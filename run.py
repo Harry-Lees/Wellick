bb65708ec00e93f3754e1aa9c75c6f4f579b6c4b
Harry Lees <harry.lees@gmail.com>
2023-02-02 18:49:16 +0000

    added coloured print output and more reliable error messages

--- run.py
+++ run.py
@@ -2,16 +2,28 @@ import os
 import pathlib
 import subprocess
 
+BLACK = "\u001b[30m"
+RED = "\u001b[31m"
+GREEN = "\u001b[32m"
+WHITE = "\u001b[97m"
+
+def cprint(text, color):
+    print(f"{color}{text}{WHITE}")
+
 outfile = pathlib.Path("a.out")
 env = {**os.environ, "RUST_BACKTRACE": "1"}
 
 if outfile.is_file():
     outfile.unlink()
 
-result = subprocess.check_call(["cargo", "run"], env=env)
-if result != 0:
-    print("Failed to compile code")
-    exit(result)
+result = subprocess.run(["cargo", "run"], env=env)
+if result.returncode != 0:
+    cprint("Failed to compile", RED)
+    exit(result.returncode)
 
 subprocess.check_call(["cc", outfile])
 subprocess.check_call(["otool", "-tvVBd", outfile])
+cprint("Successfully compiled", GREEN)
+
+result = subprocess.run(["./a.out"])
+print(result)
