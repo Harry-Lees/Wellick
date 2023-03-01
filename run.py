c965f2b3c40354c8850241370c1aac083bbfb069
Harry Lees <harry.lees@gmail.com>
2023-03-01 14:59:55 +0000

    compile Wellick compiler in release mode by default

--- run.py
+++ run.py
@@ -16,12 +16,18 @@ env = {**os.environ, "RUST_BACKTRACE": "1"}
 if outfile.is_file():
     outfile.unlink()
 
-result = subprocess.run(["cargo", "run"], env=env)
+result = subprocess.run(["cargo", "run", "--release"], env=env)
 if result.returncode != 0:
     cprint("Failed to compile", RED)
     exit(result.returncode)
 
+# compile the standard library.
+subprocess.check_call(["gcc", "-O3", "-c", "libwlk.c"])
+
+# Link the standard library with the compiled wellick program.
+# subprocess.check_call(["cc", outfile, "libwlk.o"])
 subprocess.check_call(["cc", outfile])
+
 # subprocess.check_call(["otool", "-tvVBd", outfile])
 cprint("Successfully compiled", GREEN)
 
