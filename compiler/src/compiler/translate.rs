dc5ba20c57c2af95412aa92c3c159603acc829c3
Harry Lees <harry.lees@gmail.com>
2023-03-01 14:58:05 +0000

    allowed func calls to take an expression instead of just literal values

--- compiler/src/compiler/translate.rs
+++ compiler/src/compiler/translate.rs
@@ -1,8 +1,9 @@
 use super::ast;
 use super::variables;
+
 use cranelift::prelude::AbiParam;
 use cranelift::prelude::InstBuilder;
-use cranelift_codegen::ir::{entities::Value, types};
+use cranelift_codegen::ir::{entities::Value, types, InstBuilderBase};
 use cranelift_frontend::FunctionBuilder;
 use cranelift_module::{Linkage, Module};
 use cranelift_object::ObjectModule;
@@ -56,51 +57,33 @@ impl<'a, 'b> FunctionTranslator<'a, 'b> {
     }
 
     fn translate_call(&mut self, expr: &ast::Call) -> Value {
+        println!("{:?}", self.module.get_name(expr.func.as_str()));
         let mut sig = self.module.make_signature();
+        let mut arg_values = Vec::new();
         for arg in &expr.args {
-            let arg = self
-                .variables
-                .get(&arg.ident)
-                .expect("Variable not found in scope");
-            sig.params.push(AbiParam::new(arg.var_type));
+            let func_arg = self.translate_expr(arg);
+            arg_values.push(func_arg);
+            // let arg_type = self.builder.ins().data_flow_graph().value_type(func_arg);
+            let arg_type = self.builder.func.dfg.value_type(func_arg);
+            sig.params.push(AbiParam::new(arg_type));
         }
         sig.returns.push(AbiParam::new(types::I32));
         let callee = self
             .module
             .declare_function(&expr.func, Linkage::Import, &sig)
-            .unwrap();
+            .expect("Unable to declare function");
         let local_callee = self.module.declare_func_in_func(callee, self.builder.func);
-
-        let mut arg_values = Vec::new();
-        for arg in &expr.args {
-            let arg_variable = self.variables.get(&arg.ident).unwrap().reference.clone();
-            let cloned = arg_variable.clone();
-            let arg_value = self.builder.use_var(cloned);
-            arg_values.push(arg_value);
-        }
-
         let call = self.builder.ins().call(local_callee, &arg_values);
-        let result = self.builder.inst_results(call);
-        if result.len() > 0 {
-            return result[0];
-        } else {
-            println!("{:?}", self.builder.func);
-            println!("{:?}", arg_values);
-            println!("{:?}", result);
-            return result[0];
-        }
-        // let result = self.builder.inst_results(call)[0];
+        self.builder.inst_results(call)[0]
     }
 
     fn translate_return(&mut self, expr: &ast::Expression) -> Value {
-        // println!("In translate_return {expr:?}");
         let value = self.translate_expr(expr);
         self.builder.ins().return_(&[value]);
         value
     }
 
     fn translate_assign(&mut self, expr: &ast::Assignment) -> Value {
-        // println!("Translate assign {expr:?}");
         let name = &expr.target.ident;
         let value = self.translate_expr(&expr.value);
         let var = self
