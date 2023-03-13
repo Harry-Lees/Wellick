114297ced7f0cdb8c6091920f6e2bb9ca451f777
Harry Lees <harry.lees@gmail.com>
2023-03-13 16:33:07 +0000

    added more descriptive comments to builtin functions

--- builtins.c
+++ builtins.c
@@ -1,39 +1,54 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// A collection of C functions that are linked to Wellick
+// and can be called from Wellick code. These functions act
+// as the "builtins" of Wellick.
 
+// The wellick "int" type is equivalent to a 32-bit unsigned integer
+// So all integer functions use uint32_t in their parameters and return types
+
+// Print a string to stdout
 int print(uint32_t inp) {
     return printf("%d", inp);
 }
 
+// Print a string to stdout with a newline
 int println(uint32_t inp) {
     return printf("%d\n", inp);
 }
 
+// Add two integers, return the result
 uint32_t iadd(uint32_t a, uint16_t b) {
     return a + b;
 }
 
+// Subtract two integers, return the result
 uint32_t isub(uint32_t a, uint16_t b) {
     return a - b;
 }
 
+// Multiply two integers, return the result
 uint32_t imul(uint32_t a, uint16_t b) {
     return a * b;
 }
 
+// Divide two integers, return the result
 uint32_t idiv(uint32_t a, uint16_t b) {
     return a / b;
 }
 
+// Check the equality of two integers, return 1 if true, 0 if false
 uint32_t ieq (uint32_t a, uint16_t b) {
     return a == b;
 }
 
+// Check if the first integer is less than the second, return 1 if true, 0 if false
 uint32_t ilt (uint32_t a, uint16_t b) {
     return a < b;
 }
 
+// Check if the first integer is greater than the second, return 1 if true, 0 if false
 uint32_t igt (uint32_t a, uint16_t b) {
     return a > b;
 }
