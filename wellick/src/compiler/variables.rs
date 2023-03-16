58273ceb9c93448293cd9a86bfd009efcb2b8d4b
Harry Lees <harry.lees@gmail.com>
2023-03-16 14:20:44 +0000

    split variable allocation into register and stack allocations

--- wellick/src/compiler/variables.rs
+++ wellick/src/compiler/variables.rs
@@ -1,4 +1,6 @@
 use super::ast;
+use cranelift::prelude::StackSlotData;
+use cranelift_codegen::ir::StackSlot;
 use cranelift_codegen::ir::{types, Block};
 use cranelift_frontend::FunctionBuilder;
 use cranelift_frontend::Variable as cranelift_Variable;
@@ -10,7 +12,14 @@ pub(crate) fn to_cranelift_type(t: &ast::EmptyType) -> types::Type {
     match t {
         ast::EmptyType::Float => types::F64,
         ast::EmptyType::Integer => types::I32,
+        ast::EmptyType::Pointer => types::I64,
     }
+}
+
+#[derive(Debug, Clone)]
+pub enum VarLoc {
+    StackSlot(StackSlot),
+    Register,
 }
 
 /// A Variable declaration, holds the name, type, cranelift ref,
@@ -20,6 +29,7 @@ pub struct Variable {
     pub name: String,
     pub var_type: types::Type,
     pub reference: cranelift_Variable,
+    pub loc: VarLoc,
     pub mutable: bool,
 }
 
@@ -28,6 +38,7 @@ impl Variable {
         name: String,
         var_type: types::Type,
         reference: cranelift_Variable,
+        loc: VarLoc,
         mutable: bool,
     ) -> Self {
         Self {
@@ -35,6 +46,7 @@ impl Variable {
             var_type,
             mutable,
             reference,
+            loc,
         }
     }
 }
@@ -102,7 +114,42 @@ fn declare_variables_in_stmt(
     }
 }
 
+/// Allocate a stack slot for a variable.
+/// Allocating in this way allows grabbing a pointer
+/// to a local variable.
 fn alloc(
+    name: String,
+    var_type: types::Type,
+    mutable: bool,
+    builder: &mut FunctionBuilder,
+    index: &mut usize,
+    variables: &mut HashMap<String, Variable>,
+) -> Variable {
+    if variables.contains_key(&name) {
+        panic!("Cannot re-declare variable {}", name);
+    }
+
+    let stack_slot = builder.create_sized_stack_slot(StackSlotData::new(
+        cranelift::prelude::StackSlotKind::ExplicitSlot,
+        32,
+    ));
+
+    let var = Variable::new(
+        name.clone(),
+        var_type,
+        cranelift_Variable::from_u32(*index as u32),
+        VarLoc::StackSlot(stack_slot),
+        mutable,
+    );
+    variables.insert(name, var.clone());
+    *index += 1;
+    var
+}
+
+/// Allocate a register for a variable.
+/// Allocating in this way disallows getting local pointers
+/// to variables.
+fn reg_alloc(
     name: String,
     var_type: types::Type,
     mutable: bool,
@@ -117,6 +164,7 @@ fn alloc(
         name.clone(),
         var_type,
         cranelift_Variable::from_u32(*index as u32),
+        VarLoc::Register,
         mutable,
     );
     builder.declare_var(var.reference, var.var_type);
