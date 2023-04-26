0014a32959cce08e49e851a92e99f0dea033a3ba
Harry Lees <harry.lees@gmail.com>
2023-04-26 16:05:48 +0100

    added hex_println utility function for printing numbers as hexadecimal

--- builtins.c
+++ builtins.c
@@ -10,54 +10,58 @@
 // So all integer functions use uint32_t in their parameters and return types
 
 // Print a string to stdout
-int print(uint64_t inp) {
+int32_t print(int64_t inp) {
     return printf("%" PRId64, inp);
 }
 
-int println_32(int32_t inp) {
+int32_t println_32(int32_t inp) {
     return printf("%d\n", inp);
 }
 
+int32_t hex_println(int32_t inp) {
+    return printf("0x%X\n", inp);
+}
+
 // Print a string to stdout with a newline
-int println(uint64_t inp) {
+int32_t println(int64_t inp) {
     return printf("%" PRId64 "\n", inp);
 }
 
 // Add two integers, return the result
-uint32_t iadd(uint32_t a, uint16_t b) {
+int32_t iadd(int32_t a, int32_t b) {
     return a + b;
 }
 
 // Subtract two integers, return the result
-uint32_t isub(uint32_t a, uint16_t b) {
+int32_t isub(int32_t a, int32_t b) {
     return a - b;
 }
 
 // Multiply two integers, return the result
-uint32_t imul(uint32_t a, uint16_t b) {
+int32_t imul(int32_t a, int32_t b) {
     return a * b;
 }
 
 // Divide two integers, return the result
-uint32_t idiv(uint32_t a, uint16_t b) {
+int32_t idiv(int32_t a, int32_t b) {
     return a / b;
 }
 
 // Check the equality of two integers, return 1 if true, 0 if false
-uint32_t ieq (uint32_t a, uint16_t b) {
+int32_t ieq (int32_t a, int32_t b) {
     return a == b;
 }
 
 // Check if the first integer is less than the second, return 1 if true, 0 if false
-uint32_t ilt (uint32_t a, uint16_t b) {
+int32_t ilt (int32_t a, int32_t b) {
     return a < b;
 }
 
-uint32_t ilteq (uint32_t a, uint16_t b) {
+int32_t ilteq (int32_t a, int32_t b) {
     return a <= b;
 }
 
 // Check if the first integer is greater than the second, return 1 if true, 0 if false
-uint32_t igt (uint32_t a, uint16_t b) {
+int32_t igt (int32_t a, int32_t b) {
     return a > b;
 }
